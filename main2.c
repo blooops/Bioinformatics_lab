2dc1a31eb3d384d0fe17fd8e65855603364e9294
rohit roy <rohit31roy97@gmail.com>
2018-01-30 16:42:38 +0530

    Program 3

--- main2.c
+++ main2.c
@@ -2,14 +2,16 @@
 #define NUMBER_OF_ATOMS 12000 
 #define SQ(x) ((x) * (x))
 
-void print_interaction(Atom p, Atom r) {
+void print_interaction(FILE* fout, Atom p, Atom r) {
     float distance = SQ(p.x-r.x) + SQ(p.y-r.y) + SQ(p.z-r.z); 
-    if(distance < 16.0)
-        printf("%4s%6d%4s%6d - %4s%6d%4s%6d %8.3f\n", p.atom_type, p.slno, p.residue_type, p.residue_number, r.atom_type, r.slno, r.residue_type, r.residue_number, sqrt(distance));
+    if(distance < 18.49)
+        fprintf(fout, "%4s%6d%4s%6d - %4s%6d%4s%6d %8.3f\n", p.atom_type, p.slno, p.residue_type, p.residue_number, r.atom_type, r.slno, r.residue_type, r.residue_number, sqrt(distance));
 }
 
 
 int main() {
+    FILE* fout;
+    fout = fopen( "prog2.out", "w");
     int N = get_number_of_atoms("1asy.pdb");
     Atom* atoms = read_pdb("1asy.pdb", N);
 
@@ -27,9 +29,10 @@ int main() {
     for(i=protein_starting_id; i<N; i++) {
         j = rna_starting_id;
         for(; j<protein_starting_id; j++) {
-            print_interaction(atoms[i], atoms[j]);
+            print_interaction(fout, atoms[i], atoms[j]);
         }
     }
+    fclose(fout);
 
     free(atoms);
     return 0;
--- main3.c
+++ main3.c
@@ -32,6 +32,53 @@ f2 get_average_b(Atom* atoms, int start_index, int stop_index) {
     return returns;
 
 }
+
+void print_residue_bfactor(Atom* atoms, int start, int stop) {
+    int max_id, min_id;
+    max_id = min_id = -1;
+    float max_val, min_val;
+    char max_type[10], min_type[10];
+    strcpy(max_type, "DEFAULT");
+    strcpy(min_type, "DEFAULT");
+    max_val = 0.0f;
+    min_val = 1000000.0f;
+    int current_res_id;
+    int i=start;
+    while(i<=stop) {
+        float res_avg_b=0.0f;
+        current_res_id = atoms[i].residue_number;
+        int n_res =0;
+        while(atoms[i].residue_number == current_res_id) {
+            if(!(strcmp(atoms[i].atom_type, "CA ") ==0 || strcmp(atoms[i].atom_type, "N  ") ==0 || strcmp("C  ", atoms[i].atom_type)==0 || strcmp("O  ", atoms[i].atom_type)==0)) {
+                res_avg_b += atoms[i].B;
+                n_res++;
+            }
+            i++;
+        }
+        if(n_res != 0) {
+        res_avg_b /= n_res;
+        //DEBUG
+        //DEBUGprintf("%8.3f %3d STARTS %5d ENDS %5d\n",res_avg_b, n_res, atoms[i-n_res].slno,atoms[i].slno-1 );
+        //DEBUG
+
+        if(max_val < res_avg_b) {
+            max_val = res_avg_b;
+            strcpy(max_type , atoms[i-1].residue_type);
+            max_id = current_res_id;
+        }
+        if(min_val > res_avg_b) {
+            min_val = res_avg_b;
+            strcpy(min_type,atoms[i-1].residue_type);
+            min_id = current_res_id;
+        }
+        }
+    }
+
+    printf("MAXIMUM B-FACTOR RESIDUE : %5s %5d\n", max_type, max_id);
+
+    printf("MINIMUM B-FACTOR RESIDUE : %5s %5d\n", min_type, min_id);
+}
+
 int main() {
     int N = get_number_of_atoms("1oj6.pdb");
     Atom* atoms = read_pdb("1oj6.pdb", N);
@@ -71,6 +118,17 @@ int main() {
     values = get_average_b(atoms, d, N-1);
     printf("CHAIN D: MAIN=%8.3f SIDE=%8.3f \n", values.a, values.b);
 
+    printf("CHAIN A => \n");
+    print_residue_bfactor(atoms, a, b-1);
+    
+    printf("CHAIN B => \n");
+    print_residue_bfactor(atoms, b, c-1);
+
+    printf("CHAIN C => \n");
+    print_residue_bfactor(atoms, c, d-1);
+
+    printf("CHAIN D => \n");
+    print_residue_bfactor(atoms, d, N-1);
     free(atoms);
 
 }
